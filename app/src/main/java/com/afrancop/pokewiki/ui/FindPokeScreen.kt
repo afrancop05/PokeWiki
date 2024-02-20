b1e4f24c36d84e53c63c91642040903d6ddda0d8
jozehmi <joselunisadam20@iescastelar.com>
2024-02-20 09:47:27 +0100

    Add: CardPoke and Modify donde se usa la PokeCard

--- app/src/main/java/com/afrancop/pokewiki/ui/FindPokeScreen.kt
+++ app/src/main/java/com/afrancop/pokewiki/ui/FindPokeScreen.kt
@@ -1,23 +1,9 @@
 package com.afrancop.pokewiki.ui
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.twotone.Favorite
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -27,11 +13,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import com.afrancop.pokewiki.data.local.Poke
-import com.afrancop.pokewiki.navigation.Destinations
 import com.afrancop.pokewiki.viewmodel.MainViewModel
 
 @Composable
@@ -43,7 +26,6 @@ fun FindPokeScreen(onNavSelected: (String) -> Unit, viewModel: MainViewModel) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         var name: String by rememberSaveable { mutableStateOf("") }
-        var selectedPoke: Poke? by rememberSaveable { mutableStateOf(null) }
 
         TextField(
             modifier = Modifier.padding(top = 30.dp),
@@ -57,60 +39,18 @@ fun FindPokeScreen(onNavSelected: (String) -> Unit, viewModel: MainViewModel) {
             }
         )
 
+        var foundPoke: Poke? = null
+
         Button(onClick = {
             viewModel.searchPokemonByName(name).let {
-                selectedPoke = viewModel.poke
+                foundPoke = viewModel.poke
             }
         }) {
             Text("Enviar")
         }
 
-        selectedPoke?.let { poke ->
-            var isFavorite = poke.favorite
-
-            Card(
-                border = BorderStroke(2.dp, Color.Red),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(100.dp)
-                    .padding(8.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clickable {
-                            viewModel.poke = poke
-                            onNavSelected(Destinations.DetailPokeScreen.route)
-                        }
-                ) {
-                    Image(
-                        painter = rememberAsyncImagePainter(poke.sprite),
-                        contentDescription = null,
-                        modifier = Modifier.size(100.dp)
-                    )
-                    Text(text = poke.name)
-
-                    IconButton(
-                        onClick = {
-                            isFavorite = !isFavorite
-                            if (!poke.favorite) {
-                                viewModel.favPoke(poke.id!!)
-                            } else {
-                                viewModel.unfavPoke(poke.id!!)
-                            }
-                        }
-                    ) {
-                        Icon(
-                            if (isFavorite) Icons.TwoTone.Favorite else Icons.Default.FavoriteBorder,
-                            contentDescription = if (isFavorite) "Eliminar de Favoritos" else "Añadir Favorito",
-                            tint = Color.Red,
-                            modifier = Modifier.size(30.dp)
-                        )
-                    }
-                }
-            }
+        foundPoke?.let { poke ->
+            CardPoke(onNavSelected,viewModel,poke)
         }
     }
 }
--- app/src/main/java/com/afrancop/pokewiki/ui/MainScreen.kt
+++ app/src/main/java/com/afrancop/pokewiki/ui/MainScreen.kt
@@ -1,33 +1,15 @@
 package com.afrancop.pokewiki.ui
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.twotone.Favorite
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import com.afrancop.pokewiki.data.local.Poke
-import com.afrancop.pokewiki.navigation.Destinations
 import com.afrancop.pokewiki.viewmodel.MainViewModel
 
 @Composable
@@ -44,49 +26,7 @@ fun MainScreen(onNavSelected: (String) -> Unit, viewModel: MainViewModel,page:In
             content = {
                 val pokes: List<Poke> = viewModel.pokes.value
                 itemsIndexed(pokes) { index,poke ->
-
-                    var isFavorite = poke.favorite
-
-                    Card(
-                        border = BorderStroke(2.dp, Color.Red),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(100.dp)
-                            .padding(8.dp)
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .clickable {
-                                    viewModel.poke = poke
-                                    onNavSelected(Destinations.DetailPokeScreen.route) }
-                        ) {
-                            Image(
-                                painter = rememberAsyncImagePainter(poke.sprite),
-                                contentDescription = null,
-                                modifier = Modifier.size(100.dp)
-                            )
-                            Text(text = poke.name)
-
-                            IconButton(
-                                onClick = {
-                                    isFavorite = !isFavorite
-                                    if (!poke.favorite) {
-                                        viewModel.favPoke(poke.id!!)
-                                    }else{viewModel.unfavPoke(poke.id!!)}
-                                }
-                            ) {
-                                Icon(
-                                    if (isFavorite) Icons.TwoTone.Favorite else Icons.Default.FavoriteBorder,
-                                    contentDescription = if (isFavorite) "Eliminar de Favoritos" else "Añadir Favorito",
-                                    tint = Color.Red,
-                                    modifier = Modifier.size(30.dp)
-                                )
-                            }
-                        }
-                    }
+                    CardPoke(onNavSelected,viewModel,poke)
                 }
             }
         )
